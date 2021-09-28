d17e558f630a69faeaacece3545ee057127e170d
bikesh shah <bikeshshah08@gmail.com>
2021-09-28 13:30:45 +0545

    background images

--- main.py
+++ main.py
@@ -13,3 +13,17 @@ RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
+# creating windows
+
+WINDOW_WIDTH = 700
+WINDOW_HEIGHT = 700
+
+# background image
+co = pygame.image.load("background.jpg")
+mp= pygame.image.load("back2.jpg")
+bk = pygame.image.load("snake.jpg")
+
+co= pygame.transform.scale(co,(WINDOW_WIDTH,WINDOW_HEIGHT))
+mp= pygame.transform.scale(mp,(WINDOW_WIDTH,WINDOW_HEIGHT))
+bk = pygame.transform.scale(bk,(WINDOW_WIDTH,WINDOW_HEIGHT))
+
