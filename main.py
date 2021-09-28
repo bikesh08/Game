12df25b25e6653a6e20daad6db17ae3122e5a4d0
bikesh shah <bikeshshah08@gmail.com>
2021-09-28 13:35:49 +0545

    intial commmit

--- main.py
+++ main.py
@@ -27,3 +27,17 @@ co= pygame.transform.scale(co,(WINDOW_WIDTH,WINDOW_HEIGHT))
 mp= pygame.transform.scale(mp,(WINDOW_WIDTH,WINDOW_HEIGHT))
 bk = pygame.transform.scale(bk,(WINDOW_WIDTH,WINDOW_HEIGHT))
 
+
+VELOCITY = 10
+SNAKE_WIDTH = 15
+APPLE_SIZE = 20
+TOP_WIDTH = 40
+small_font = pygame.font.SysFont('forte', 25)
+medium_font = pygame.font.SysFont('showcard gothic', 30, True)
+large_font = pygame.font.SysFont('chiller', 60, True, True)
+clock = pygame.time.Clock()
+
+canvas = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('Snake Game')
+
+
