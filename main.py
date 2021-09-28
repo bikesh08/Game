3064c404345cce93e403c2b849e72d09f9da7d4c
bikesh shah <bikeshshah08@gmail.com>
2021-09-28 13:18:36 +0545

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#includes pause
+import pygame
+import sys
+import random
+
+pygame.init()
+FPS = 30
+#colours
+
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
