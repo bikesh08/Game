4cff1c80243f9f640b189319f07926058a3eb0c1
bikesh shah <bikeshshah08@gmail.com>
2021-09-28 13:56:14 +0545

    intial commmit

--- main.py
+++ main.py
@@ -51,4 +51,65 @@ pygame.mixer.music.load('bg_music_1.mp3')
 pygame.mixer.music.play(1)
 
 
+def start_game():
+    canvas.fill(BLACK)
+    canvas.blit(bk,(0,0))
+    start_font1 = large_font.render("Welcome To Snake Game", True,BLUE,RED)
+    start_font2 = large_font.render("Play Game", True, RED, YELLOW)
+    start_font3 = large_font.render("Rules", True, RED, YELLOW)
+    start_font4 = large_font.render("Quit", True, RED, YELLOW)
+    start_font5 = large_font.render("Creator", True, RED, YELLOW)
+    start_font1_rect = start_font1.get_rect()
+    start_font2_rect = start_font2.get_rect()
+    start_font3_rect = start_font3.get_rect()
+    start_font4_rect = start_font4.get_rect()
+    start_font5_rect = start_font5.get_rect()
+
+    start_font1_rect.center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2 - 200)
+    start_font2_rect.center = (WINDOW_WIDTH/2 + 100, WINDOW_HEIGHT/2 + 50)
+    start_font3_rect.center = (WINDOW_WIDTH/2 + 100, WINDOW_HEIGHT / 2 + 100)
+    start_font5_rect.center = (WINDOW_WIDTH/2 + 100, WINDOW_HEIGHT / 2 + 150)
+    start_font4_rect.center = (WINDOW_WIDTH/2 + 100, WINDOW_HEIGHT/2 + 200)
+
+    canvas.blit(start_font1, start_font1_rect)
+    canvas.blit(start_font2, start_font2_rect)
+    canvas.blit(start_font3, start_font3_rect)
+    canvas.blit(start_font4, start_font4_rect)
+    canvas.blit(start_font5, start_font5_rect)
+    pygame.display.update()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:
+                    gameloop()
+                if event.key == pygame.K_q:
+                    pygame.quit()
+                    sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = event.pos
+                if x > start_font3_rect.left and x < start_font3_rect.right:
+                    if y > start_font3_rect.top and y < start_font3_rect.bottom:
+                        start_inst(start_font1, start_font1_rect)
+                if x > start_font2_rect.left and x < start_font2_rect.right:
+                    if y > start_font2_rect.top and y < start_font2_rect.bottom:
+                        gameloop()
+                if x > start_font5_rect.left and x < start_font5_rect.right:
+                    if y > start_font5_rect.top and y < start_font5_rect.bottom:
+                        creator()
+                if x > start_font4_rect.left and x < start_font4_rect.right:
+                    if y > start_font4_rect.top and y < start_font4_rect.bottom:
+                        pygame.quit()
+                        sys.exit()
+
+
+
+        pygame.display.update()
+
+
+
+
+
 
